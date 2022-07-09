f144f99f04a5490321221c56ce3c507870c94046
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2022-07-09 18:21:35 +0300

    Ветка webstorm

--- src/USER/USER.tsx
+++ src/USER/USER.tsx
@@ -4,6 +4,7 @@ export const USER = () => {
     return (
         <div>
             <h2>User10</h2>
+            <p>а я из ветки, созданной средствами Webstorm без командной строки</p>
         </div>
     )
 }
